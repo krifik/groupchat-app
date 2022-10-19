4f30396d284cb0d96136961ad8d7c968c101891a
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2022-10-19 17:30:57 +0700

    change return contract

--- backend/repository/message_repository.go
+++ backend/repository/message_repository.go
@@ -2,9 +2,10 @@ package repository
 
 import (
 	"mangojek-backend/entity"
+	"mangojek-backend/model"
 )
 
 type MessageRepository interface {
 	SendMessage(message *entity.Message)
-	GetMessages() (messages []entity.Message)
+	GetMessages() (createMessageResponse []model.CreateMessageResponse)
 }
