a1152a22b2c1ab26ecac0a437848594c5fa4e9b4
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2022-10-19 17:30:03 +0700

    add field messages in response and request struct

--- backend/model/user_model.go
+++ backend/model/user_model.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"mangojek-backend/entity"
 	"time"
 
 	"gorm.io/gorm"
@@ -9,32 +10,35 @@ import (
 type CreateUserRequest struct {
 	Id int `json:"id"`
 	// FirstName string `json:"first_name"`
-	Name      string         `json:"name"`
-	Email     string         `json:"email"`
-	Password  string         `json:"password"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at"`
+	Name      string           `json:"name"`
+	Email     string           `json:"email"`
+	Password  string           `json:"password"`
+	CreatedAt time.Time        `json:"created_at"`
+	UpdatedAt time.Time        `json:"updated_at"`
+	DeletedAt gorm.DeletedAt   `json:"deleted_at"`
+	Messages  []entity.Message `json:"messages"`
 }
 
 type CreateUserResponse struct {
 	Id int `json:"id"`
 	// FirstName string `json:"first_name"`
-	Name      string         `json:"name"`
-	Email     string         `json:"email"`
-	Password  string         `json:"-"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at"`
+	Name      string           `json:"name"`
+	Email     string           `json:"email"`
+	Password  string           `json:"-"`
+	CreatedAt time.Time        `json:"created_at"`
+	UpdatedAt time.Time        `json:"updated_at"`
+	DeletedAt gorm.DeletedAt   `json:"deleted_at"`
+	Messages  []entity.Message `json:"messages"`
 }
 
 type GetUserResponse struct {
 	Id int `json:"id"`
 	// FirstName string `json:"first_name"`
-	Name      string         `json:"name"`
-	Email     string         `json:"email"`
-	Password  string         `json:"-"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at"`
+	Name      string           `json:"name"`
+	Email     string           `json:"email"`
+	Password  string           `json:"-"`
+	CreatedAt time.Time        `json:"created_at"`
+	UpdatedAt time.Time        `json:"updated_at"`
+	DeletedAt gorm.DeletedAt   `json:"deleted_at"`
+	Messages  []entity.Message `json:"messages"`
 }
