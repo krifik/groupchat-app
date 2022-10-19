2083424a5345fbdad38c7574fb6bfbd3ad6e7c62
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2022-10-19 17:31:46 +0700

    remove temp middleware in route

--- backend/routes/router.go
+++ backend/routes/router.go
@@ -2,16 +2,15 @@ package routes
 
 import (
 	"mangojek-backend/controller"
-	"mangojek-backend/middleware"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func Route(app *fiber.App, userController controller.UserController, messageController controller.MessageController) {
-	app.Get("/api/users", middleware.AuthMiddleware, userController.FindAll)
+	app.Get("/api/users", userController.FindAll)
 	app.Post("/api/register", userController.Register)
 	app.Post("/api/login", userController.Login)
-	app.Post("/api/message", middleware.AuthMiddleware, messageController.SendMessage)
-	app.Get("/api/messages", middleware.AuthMiddleware, messageController.GetMessages)
+	app.Post("/api/message", messageController.SendMessage)
+	app.Get("/api/messages", messageController.GetMessages)
 	// app.Post("/api/users", middleware.AuthMiddleware, controller.Insert)
 }
