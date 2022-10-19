07c2a3b75abe1311bab62ac6e86748658bc0d8ad
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2022-10-19 17:33:43 +0700

    make a helper function for pusher

--- frontend/src/helper/pusher.js
+++ frontend/src/helper/pusher.js
@@ -1,12 +1,9 @@
+import Pusher from "pusher-js";
+
 Pusher.logToConsole = true;
 
 var pusher = new Pusher("711add371e9613586624", {
   cluster: "ap1",
 });
 
-var channel = pusher.subscribe("group-chat");
-channel.bind("groupchat", function (data) {
-  // app.messages.push(JSON.stringify(data));
-});
-
-export default channel;
+export default pusher;
