881ad83dd91a762b344fd32508d2b8ed866ee548
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2022-10-28 02:21:04 +0700

    add get user contract

--- backend/service/user_service.go
+++ backend/service/user_service.go
@@ -8,4 +8,5 @@ type UserService interface {
 	Register(request model.CreateUserRequest) (response model.CreateUserResponse, err error)
 	FindAll() ([]model.GetUserResponse, error)
 	Login(request model.CreateUserRequest) (response model.CreateUserResponse, err error)
+	GetUser(token string) (response model.GetUserResponse)
 }
