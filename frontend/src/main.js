c3c1e5b20be8e72745d5cd3f7fa63293496fcb91
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2022-10-28 02:19:54 +0700

    add pinia

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
 import router from "../src/routes/index.js";
+import { createPinia } from "pinia";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(createPinia()).mount("#app");
